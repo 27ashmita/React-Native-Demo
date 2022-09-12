0bb542d6b559136848ad4dc3639a28a898ccaf20
27ashmita <ashmitaghimire1@gmail.com>
2022-09-12 16:38:28 +0530

    changes on settings screen

--- src/components/screens/Settings/index.tsx
+++ src/components/screens/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,11 +9,36 @@ import {
   Image,
 } from 'react-native';
 import AppButton from '../../../components/Button';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'react-native-image-picker';
 
-const logo = require('../../../../assets/Login/q.png');
+import {ImagePickerHeader} from '../../ImagePickerHeader';
+import {ImagePickerModal} from '../../ImagePickerModal';
+import {ImagePickerAvatar} from '../../ImagePickerAvatar';
 
 const Settings = () => {
+  const [pickerResponse, setPickerResponse] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  const onImageLibraryPress = useCallback(() => {
+    const options = {
+      selectionLimit: 1,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchImageLibrary(options, setPickerResponse);
+  }, []);
+
+  const onCameraPress = useCallback(() => {
+    const options = {
+      saveToPhotos: true,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchCamera(options, setPickerResponse);
+  }, []);
+
+  const uri = pickerResponse?.assets && pickerResponse.assets[0].uri;
+
   return (
     <SafeAreaView style={styles.containerView}>
       <View style={{flex: 1, backgroundColor: 'green'}}>
@@ -28,66 +53,50 @@ const Settings = () => {
         </Text>
         <View></View>
       </View>
-      <View
-        style={{
+      <View style={{
           flex: 2,
           backgroundColor: 'white',
           borderRadius: 8,
           margin: 8,
-          marginTop: -32,
+          marginTop: -20,
         }}>
-        <View
+        {/* <ImagePickerHeader /> */}
+        <ImagePickerAvatar uri={uri} onPress={() => setVisible(true)} />
+        <ImagePickerModal
+          isVisible={visible}
+          onClose={() => setVisible(false)}
+          onImageLibraryPress={onImageLibraryPress}
+          onCameraPress={onCameraPress}
+        />
+      
+      <View style={{marginTop: 10}}>
+        <TextInput
+          placeholder="ニックネーム"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          autoCorrect={false}
+          wrapperStyles={styles.emailInput}
+          style={styles.textInput}
+        />
+        <Text
           style={{
-            width: 200,
-            height: 200,
+            color: 'black',
             alignSelf: 'center',
-            marginTop: -100,
+            fontSize: 20,
+            padding: 16,
           }}>
-          <Image
-            source={logo}
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              alignSelf: 'center',
-              width: 200,
-              height: 200,
-              borderRadius: 100,
-            }}
-          />
-          <TouchableOpacity
-            // onPress={addImage}
-            style={styles.uploadBtn}>
-            <Text>Upload Image</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={{marginTop: 16}}>
-          <TextInput
-            placeholder="ニックネーム"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            autoCorrect={false}
-            wrapperStyles={styles.emailInput}
-            style={styles.textInput}
-          />
-          <Text
-            style={{
-              color: 'black',
-              alignSelf: 'center',
-              fontSize: 20,
-              padding: 16
-            }}>
-            ※ ニックネームは後から変更できます ※ 4 〜 20文字以内
-          </Text>
-          <AppButton
-            title="設定する"
-            size="sm"
-            backgroundColor="#FFE100"
-            color="black"
-            style={{margin: 10}}
-          />
-        </View>
+          ※ ニックネームは後から変更できます ※ 4 〜 20文字以内
+        </Text>
+        <AppButton
+          title="設定する"
+          size="sm"
+          backgroundColor="#FFE100"
+          color="black"
+          style={{margin: 10}}
+        />
       </View>
+      </View>
+      {/* </View> */}
     </SafeAreaView>
   );
 };
@@ -122,8 +131,12 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 8,
     backgroundColor: 'white',
-    marginTop: 42
-  }
+    marginTop: 42,
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: '#f2f2fC',
+  },
 });
 
 export default Settings;
